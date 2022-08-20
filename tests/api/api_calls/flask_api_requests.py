3f3b0397cc3297c350e853f217a68056cae76220
Sony George <sony.george@nuance.com>
2022-08-20 17:29:27 +0530

    - Flask API calls

---
+++ tests/api/api_calls/flask_api_requests.py
@@ -0,0 +1,51 @@
+import json
+import requests
+from tests.api.config import *
+
+def get_users():
+  response = requests.get(users_url)
+  return response
+
+
+def get_token(username, password):
+  response = requests.get(auth_url, auth=(username, password))
+  return response
+
+
+def return_token(username, password):
+  return get_token(username, password).json()['token']
+
+
+def get_specific_user(user):
+  headers = {'Token': return_token(username, password)}
+  url = users_url + '/' + user
+
+  response = requests.get(url, headers=headers)
+  return response
+
+
+def update_users(user):
+  url = users_url + '/' + user
+  headers = {'Token': return_token(username, password)}
+  response = requests.get(url, headers=headers, data=update_payload)
+  return response
+
+
+def add_users():
+  headers = {'Content-Type': 'application/json'}
+  response = requests.post(users_url, headers=headers, data=json.dumps(register_user_payload))
+  return response
+
+
+response = get_users()
+response = get_token(username,password)
+response = get_specific_user(username)
+response = update_users(username)
+response = add_users()
+
+print(response.status_code)
+print(response.json())
+print(response.headers)
+
+token = return_token(username, password)
+print(token)
---
+++ tests/api/config.py
@@ -0,0 +1,20 @@
+url = 'http://localhost:8080/'
+auth_url = url + "api/auth/token"
+users_url = url + "api/users"
+
+username = "test_user5"
+password = "test_pass"
+
+update_payload = {
+  "firstname": "first1_1",
+  "lastname": "last1_1",
+  "phone": 123456789011
+}
+
+register_user_payload = {
+    "username": "test_user19",
+    "password": "test_pass",
+    "firstname": "first",
+    "lastname": "last",
+    "phone": 1234567890
+  }
