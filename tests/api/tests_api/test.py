4c6238709d21707f7e2b20339969c4681588fc1e
Sony George <sony.george@nuance.com>
2022-08-21 16:15:59 +0530

    - pytest api file added

---
+++ tests/api/tests_api/test.py
@@ -0,0 +1,53 @@
+from tests.api.request_functions.flask_api_requests import *
+from tests.api.config import *
+
+
+def test_get_users():
+  response = get_users()
+  arr_users = response.json()['payload']
+  assert response.status_code == 200
+  assert 'root' in arr_users
+  assert 'test_user1' in arr_users
+
+  assert 'test_user1' in arr_users
+
+
+def test_get_specific_users():
+  response = get_specific_user(username)
+  user_info = response.json()['payload']
+  assert response.status_code == 200
+  assert response.json()['message'] == 'retrieval succesful'
+  assert response.json()['status'] == 'SUCCESS'
+  assert 'first' in user_info['firstname']
+  assert 'last' in user_info['lastname']
+  assert '1234567890' in user_info['phone']
+
+
+def test_get_token():
+  response = get_token(username, password)
+  assert response.status_code == 200
+  assert response.json()['status'] == 'SUCCESS'
+  token = response.json()['token']
+  assert len(token) >= 50
+
+  # response = update_users(username)
+  # response = add_users()
+
+
+def test_update_users():
+  response = update_users(username)
+  user_info = response.json()['payload']
+
+  assert response.status_code == 200
+  assert response.json()['message'] == 'retrieval succesful'
+  assert response.json()['status'] == 'SUCCESS'
+  assert 'first' in user_info['firstname']
+  assert 'last' in user_info['lastname']
+  assert '1234567890' in user_info['phone']
+
+
+def test_add_user():
+  response = add_user()
+  assert response.status_code == 201
+  assert response.json()['status'] == 'SUCCESS'
+  assert response.json()['message'] == 'Created'
